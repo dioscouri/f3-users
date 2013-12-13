e83d316b78c01677c628914334971ff92f41ff43
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2013-12-13 16:22:51 -0500

    initial commit - placeholders

---
+++ src/Users/Listener.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Users;
+
+class Listener extends \Prefab 
+{
+    public function onSystemRebuildMenu( $event )
+    {
+        if ($mapper = $event->getArgument('mapper')) 
+        {
+
+        }
+        
+    }
+}
