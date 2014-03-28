bad5280b9608bb35b5bd01ee7d85fc4465f7c2b8
Chris French <chris@ammonitenetworks.com>
2014-03-28 15:23:58 -0600

    hybrid fixes

--- src/Users/Site/Views/login/hybrid.php
+++ src/Users/Site/Views/login/hybrid.php
@@ -1,9 +1,9 @@
 <fieldset>
             <legend>Use service</legend>
-            &nbsp;&nbsp;<a href="/social/auth/google">Sign-in with Google</a><br /> 
-            &nbsp;&nbsp;<a href="/social/auth/yahoo">Sign-in with Yahoo</a><br /> 
-            &nbsp;&nbsp;<a href="/social/auth/facebook">Sign-in with Facebook</a><br />
-            &nbsp;&nbsp;<a href="/social/auth/twitter">Sign-in with Twitter</a><br />  
-            &nbsp;&nbsp;<a href="/social/auth/linkedin">Sign-in with LinkedIn</a><br />
+            &nbsp;&nbsp;<a href="/login/social/auth/google">Sign-in with Google</a><br /> 
+            &nbsp;&nbsp;<a href="/login/social/auth/yahoo">Sign-in with Yahoo</a><br /> 
+            &nbsp;&nbsp;<a href="/login/social/auth/facebook">Sign-in with Facebook</a><br />
+            &nbsp;&nbsp;<a href="/login/social/auth/twitter">Sign-in with Twitter</a><br />  
+            &nbsp;&nbsp;<a href="/login/social/auth/linkedin">Sign-in with LinkedIn</a><br />
         </fieldset>
 	    	     <br/>
