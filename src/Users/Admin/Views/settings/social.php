3624bc67bf25b32d7515e7b7428070bb8fd109f2
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-20 22:51:49 -0400

    just formatting change

--- src/Users/Admin/Views/settings/social.php
+++ src/Users/Admin/Views/settings/social.php
@@ -1,8 +1,8 @@
 <h3 class="">Social Settings</h3>
-
+<hr />
 
 <div class="form-group">
-    
+
 
     <div class="col-md-7">
         <label>Base URL</label>
@@ -12,41 +12,33 @@
         <?php endif; ?>
         <h2>Providers</h2>
         <fieldset>
-        <legend>Facebook</legend>
-        <label>Facebook enabled</label>
-        <label class="radio-inline">
-            <input type="radio" name="social[providers][Facebook][enabled]" value="false" <?php if ($flash->old('social.providers.Facebook.enabled')) { echo "checked"; } ?>> False
-        </label>
-        <label class="radio-inline">
-            <input type="radio" name="social[providers][Facebook][enabled]" value="true" <?php if ($flash->old('social.providers.Facebook.enabled')) { echo "checked"; } ?>> True
-        </label>
-        <div>
-        <label>Facebook App ID</label>
-        <input type="text" name="social[providers][Facebook][keys][id]" value="<?php echo $flash->old('social.providers.Facebook.keys.id'); ?>" >
-        </div>
-        <div>
-        <label>Facebook App secret</label>
-        <input type="text" name="social[providers][Facebook][keys][secret]" value="<?php echo $flash->old('social.providers.Facebook.keys.secret'); ?>" >
-        </div>
+            <legend>Facebook</legend>
+            <label>Facebook enabled</label> <label class="radio-inline"> <input type="radio" name="social[providers][Facebook][enabled]" value="false" <?php if ($flash->old('social.providers.Facebook.enabled')) { echo "checked"; } ?>> False
+            </label> <label class="radio-inline"> <input type="radio" name="social[providers][Facebook][enabled]" value="true" <?php if ($flash->old('social.providers.Facebook.enabled')) { echo "checked"; } ?>> True
+            </label>
+            <div>
+                <label>Facebook App ID</label>
+                <input type="text" name="social[providers][Facebook][keys][id]" value="<?php echo $flash->old('social.providers.Facebook.keys.id'); ?>">
+            </div>
+            <div>
+                <label>Facebook App secret</label>
+                <input type="text" name="social[providers][Facebook][keys][secret]" value="<?php echo $flash->old('social.providers.Facebook.keys.secret'); ?>">
+            </div>
         </fieldset>
         <fieldset>
-        <legend>Twitter</legend>
-        <label>Twitter enabled</label>
-         <label class="radio-inline">
-            <input type="radio" name="social[providers][Twitter][enabled]" value="false" <?php if ($flash->old('social.providers.Twitter.enabled')) { echo "checked"; } ?>> False
-        </label>
-        <label class="radio-inline">
-            <input type="radio" name="social[providers][Twitter][enabled]" value="true" <?php if ($flash->old('social.providers.Twitter.enabled')) { echo "checked"; } ?>> True
-        </label> 
-        <div>
-        <label>Twitter App ID</label>
-        <input type="text" name="social[providers][Twitter][keys][id]" value="<?php echo $flash->old('social.providers.Twitter.keys.id'); ?>" >
-        </div>
-        <div>
-        <label>Twitter App secret</label>
-        <input type="text" name="social[providers][Twitter][keys][secret]" value="<?php echo $flash->old('social.providers.Twitter.keys.secret'); ?>" >
-        </div>
+            <legend>Twitter</legend>
+            <label>Twitter enabled</label> <label class="radio-inline"> <input type="radio" name="social[providers][Twitter][enabled]" value="false" <?php if ($flash->old('social.providers.Twitter.enabled')) { echo "checked"; } ?>> False
+            </label> <label class="radio-inline"> <input type="radio" name="social[providers][Twitter][enabled]" value="true" <?php if ($flash->old('social.providers.Twitter.enabled')) { echo "checked"; } ?>> True
+            </label>
+            <div>
+                <label>Twitter App ID</label>
+                <input type="text" name="social[providers][Twitter][keys][id]" value="<?php echo $flash->old('social.providers.Twitter.keys.id'); ?>">
+            </div>
+            <div>
+                <label>Twitter App secret</label>
+                <input type="text" name="social[providers][Twitter][keys][secret]" value="<?php echo $flash->old('social.providers.Twitter.keys.secret'); ?>">
+            </div>
         </fieldset>
-        
+
     </div>
 </div>
