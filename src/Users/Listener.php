960e1f3c5ba696dbb14fc9f352e69cc7eb4d22f0
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-17 10:08:41 -0400

    updated menu item adder

--- src/Users/Listener.php
+++ src/Users/Listener.php
@@ -1,37 +1,51 @@
-<?php 
+<?php
 namespace Users;
 
-class Listener extends \Prefab 
+class Listener extends \Prefab
 {
-    public function onSystemRebuildMenu( $event )
+
+    public function onSystemRebuildMenu($event)
     {
-		if ($model = $event->getArgument('model'))
-		{
-			$root = $event->getArgument( 'root' );
-			$users = clone $model;
-        		 
-			$users->insert(
-					array(
-						'type'	=> 'admin.nav',
-						'priority' => 50,
-						'title'	=> 'Users',
-						'icon'	=> 'fa fa-user',
-        				'is_root' => false,
-						'tree'	=> $root,
-						'base' => '/admin/users',
-					)
-				);
-        	
-			$children = array(
-        			array( 'title'=>'List', 'route'=>'/admin/users', 'icon'=>'fa fa-user' ),
-                    array( 'title'=>'Groups', 'route'=>'/admin/users/groups', 'icon'=>'fa fa-group' ),
-                    array( 'title'=>'Roles', 'route'=>'/admin/users/roles', 'icon'=>'fa fa-unlock' ),
-        	        array( 'title'=>'Settings', 'route'=>'/admin/users/settings', 'icon'=>'fa fa-cogs' ),
-			);
-       		$users->addChildrenItems( $children, $root );
-        	
-        	\Dsc\System::instance()->addMessage('Users added its admin menu items.');
+        if ($model = $event->getArgument('model'))
+        {
+            $root = $event->getArgument('root');
+            $users = clone $model;
+            
+            $users->insert(array(
+                'type' => 'admin.nav',
+                'priority' => 50,
+                'title' => 'Users',
+                'icon' => 'fa fa-user',
+                'is_root' => false,
+                'tree' => $root,
+                'base' => '/admin/users'
+            ));
+            
+            $children = array(
+                array(
+                    'title' => 'List',
+                    'route' => '/admin/users',
+                    'icon' => 'fa fa-user'
+                ),
+                array(
+                    'title' => 'Groups',
+                    'route' => '/admin/users/groups',
+                    'icon' => 'fa fa-group'
+                ),
+                array(
+                    'title' => 'Roles',
+                    'route' => '/admin/users/roles',
+                    'icon' => 'fa fa-unlock'
+                ),
+                array(
+                    'title' => 'Settings',
+                    'route' => '/admin/users/settings',
+                    'icon' => 'fa fa-cogs'
+                )
+            );
+            $users->addChildrenItems($children, $root);
+            
+            \Dsc\System::instance()->addMessage('Users added its admin menu items.');
         }
-        
     }
 }
