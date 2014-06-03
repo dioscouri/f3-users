66d4a2306c0b8ad7c1f5c4f6995f82cea57919f8
Chris French <chris@ammonitenetworks.com>
2014-06-02 21:15:37 -0700

    Groups checkboxes labels was using the wrong varible

--- src/Users/Admin/Views/groups/checkboxes.php
+++ src/Users/Admin/Views/groups/checkboxes.php
@@ -1,11 +1,12 @@
 <?php if (!empty($groups)) { ?>
+
 <div class="max-height-200 list-group-item">
 	<?php $current = \Joomla\Utilities\ArrayHelper::getColumn( (array) $flash->old('groups'), 'id' ); ?>
     <?php foreach ($groups as $one) { ?>
     <div class="checkbox">
         <label>
             <input type="checkbox" name="groups[]" class="icheck-input" value="<?php echo $one->_id; ?>" <?php if (in_array($one->_id, $current)) { echo "checked='checked'"; } ?>>
-            <?php echo $one->name;  ?>
+            <?php echo $one->title;  ?>
         </label>
     </div>
     <?php } ?> 
