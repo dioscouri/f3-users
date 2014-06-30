bfb1a407ad20388094431921066fd6f8d24ebb57
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-30 09:32:01 -0400

    curated priority of search scopes

--- bootstrap.php
+++ bootstrap.php
@@ -2,32 +2,35 @@
 class UsersBootstrap extends \Dsc\Bootstrap
 {
     protected $dir = __DIR__;
+
     protected $namespace = 'Users';
 
     protected function runSite()
     {
         parent::runSite();
         
-        \Dsc\System::instance()->get('router')->mount( new \Users\Site\Routes\Prefixed, $this->namespace );
+        \Dsc\System::instance()->get('router')->mount(new \Users\Site\Routes\Prefixed(), $this->namespace);
     }
-    
+
     protected function preSite()
     {
         if (class_exists('\Minify\Factory'))
         {
-            \Minify\Factory::registerPath( $this->dir . "/src/" );
+            \Minify\Factory::registerPath($this->dir . "/src/");
         }
     }
-    
+
     protected function preAdmin()
     {
         if (class_exists('\Search\Factory'))
         {
-            \Search\Factory::registerSource( new \Search\Models\Source( array(
-                'id'=>'users', 'title'=>'Users', 'class'=>'\Users\Models\Users'
-            ) ) );
-            
-        }        
+            \Search\Factory::registerSource(new \Search\Models\Source(array(
+                'id' => 'users',
+                'title' => 'Users',
+                'class' => '\Users\Models\Users',
+                'priority' => 20,
+            )));
+        }
     }
 }
 $app = new UsersBootstrap();
